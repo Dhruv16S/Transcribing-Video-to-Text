8b2b6c50da521b9b6045e3e5627979cafcebfaf9
Dhruv Saxena <dhruv162002@gmail.com>
2023-12-14 21:57:54 +0530

    Modify saving transcript

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from utils import load_wav2vec2_asr_model, transcribe_audio
+import os
 
 model_path = 'model.pth'
 model = load_wav2vec2_asr_model(model_path)
@@ -11,6 +12,10 @@ audio_path = './audio/original/audio_extracted.mp3'
 temp = transcribe_audio(model, audio_path)
 transcript += f"Audio that has not been cleaned: {temp}\n"
 
-output_file = './transcript.txt'
+output_directory = "./output"
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
+
+output_file = f'{output_directory}/{len(os.listdir(output_directory))}_transcript.txt'
 with open(output_file, 'w+') as file:
     file.write(transcript)
