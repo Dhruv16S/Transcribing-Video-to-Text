ce016d5b71ad4b6b3d369575554ad92262657d6d
Dhruv Saxena <dhruv162002@gmail.com>
2023-12-18 12:37:41 +0530

    Ready for deployment

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-# Similar functionality as main.py, but for web app
-
-from flask import Flask, render_template, request, send_from_directory
+import streamlit as st
 import os
 import zipfile
-import subprocess
-from werkzeug.utils import secure_filename
 import shutil
 import whisper
 from moviepy.editor import *
@@ -12,6 +8,8 @@ import librosa
 from pydub import AudioSegment
 import noisereduce as nr
 from scipy.io import wavfile
+import streamlit_ext as ste
+
 
 def extract_audio(video_path):
     output_directory = "./audio/original"
@@ -34,26 +32,14 @@ def noise_removal():
     audio = AudioSegment.from_wav(f'{output_directory}/cleaned_audio.wav')
     audio.export(f'{output_directory}/cleaned_audio.mp3', format='mp3')
 
-app = Flask(__name__)
-
-UPLOAD_FOLDER = './videos'
-ALLOWED_EXTENSIONS = {'mp4'}
-TRANSCRIPT_OUTPUT_FILE = './output/whisper_transcript.txt'
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 
-
-def allowed_file(filename):
-    return '.' in filename and filename.split('.')[1].lower() in ALLOWED_EXTENSIONS
-
-def generate_files(video_path):
-
-    # gen_command = f"bash ./generate_transcripts.sh {video_path}"
-    # subprocess.run(gen_command, shell=True, capture_output=True, text=True)
-    # Python implementation of above codes
+def generate_files(uploaded_file):
+    video_path = "./temp_video.mp4"
+    with open(video_path, "wb") as video_file:
+        video_file.write(uploaded_file.read())
+    st.video(video_path)
+    st.write("Transcribing files. Please wait")
     extract_audio(video_path)
     noise_removal()
-
     try:
         model = whisper.load_model("base")
         result = model.transcribe("./audio/cleaned/cleaned_audio.mp3")["text"]
@@ -62,75 +48,41 @@ def generate_files(video_path):
     output_directory = "./output"
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
-    with open(TRANSCRIPT_OUTPUT_FILE, 'w+') as file:
+    with open('./output/whisper_transcript.txt', 'w+') as file:
         file.write(result)
 
-    # copy_command = f"bash ./copy_directory.sh"
-    # subprocess.run(copy_command, shell=True, capture_output=True, text=True)
-    shutil.move('./audio', './static/')
-
-    original_audio_path = 'static/audio/original/audio_extracted.mp3'
-    cleaned_audio_path = 'static/audio/cleaned/cleaned_audio.mp3'
+    original_audio_path = './audio/original/audio_extracted.mp3'
+    cleaned_audio_path = './audio/cleaned/cleaned_audio.mp3'
     with open('./output/whisper_transcript.txt', 'r') as file:
         transcript = file.read()
 
+    os.remove(video_path)
+
     return original_audio_path, cleaned_audio_path, transcript
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    return render_template('index.html', original_audio=None,
-                                   cleaned_audio=None, transcript=None)
+def allowed_file(filename):
+    return '.' in filename and filename.split('.')[1].lower() in {'mp4'}
 
-@app.route('/generate', methods=['GET', 'POST'])
-def generate():
-    output_directory = "./videos"
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
-    if request.method == 'POST':
-        file = request.files['formFile']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file_path = f"{UPLOAD_FOLDER}/{filename}"
-            file.save(file_path)
+def main():
+    st.title("Transcribing Videos to Text")
+    uploaded_file = st.file_uploader("Choose a video", type=["mp4"])
+    if uploaded_file is not None:
+        if allowed_file(uploaded_file.name):
+            original_audio, cleaned_audio, transcript = generate_files(uploaded_file)
+            st.write("Original Audio file")
+            st.audio(original_audio, format='audio/mp3', start_time=0)
+            st.write("Cleaned Audio file")
+            st.audio(cleaned_audio, format='audio/mp3', start_time=0)
+            st.text_area("Transcribed Text", transcript)
+            with zipfile.ZipFile('transcribed_files.zip', 'w') as zipf:
+                zipf.write(original_audio, os.path.basename(original_audio))
+                zipf.write(cleaned_audio, os.path.basename(cleaned_audio))
+                zipf.write('./output/whisper_transcript.txt', 'transcript.txt')
+            shutil.rmtree('./audio')
+            shutil.rmtree('./output')
+            with open('transcribed_files.zip', 'rb') as file:
+                content = file.read()
+            ste.download_button(label="Download Files", data=content, file_name='transcribed_files.zip')
 
-            original_audio, cleaned_audio, transcript = generate_files(file_path)
-
-            if os.path.exists(UPLOAD_FOLDER):
-                for item in os.listdir(UPLOAD_FOLDER):
-                    item_path = os.path.join(UPLOAD_FOLDER, item)
-                    os.remove(item_path)
-
-            # return render_template('index.html', original_audio=original_audio,
-            #                        cleaned_audio=cleaned_audio, transcript=transcript)
-            return {
-                "original_audio": original_audio,
-                "cleaned_audio": cleaned_audio,
-                "transcript": transcript
-            }
-
-    return {
-        "original_audio":None,
-        "cleaned_audio":None, 
-        "transcript":None
-        }
-
-@app.route('/download', methods=['GET'])
-def download():
-    original_audio_path = './static/audio/original/audio_extracted.mp3'
-    cleaned_audio_path = './static/audio/cleaned/cleaned_audio.mp3'
-    transcript_path = './output/whisper_transcript.txt'
-
-    with zipfile.ZipFile('generated_files.zip', 'w') as zipf:
-        zipf.write(original_audio_path, os.path.basename(original_audio_path))
-        zipf.write(cleaned_audio_path, os.path.basename(cleaned_audio_path))
-        zipf.write(transcript_path, os.path.basename(transcript_path))
-
-    # Cleaning up directories
-    shutil.rmtree('./videos')
-    shutil.rmtree('./output')
-    shutil.rmtree('./static/audio')
-
-    return send_from_directory('.', 'generated_files.zip', as_attachment=True)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    main()
